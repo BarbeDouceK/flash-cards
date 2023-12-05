0c588a1d90bc2eb6da537de5a6f2520c9d6a001f
BarbeDouce <baptiste.louerat44@gmail.com>
2023-12-05 22:24:55 +0100

    Commit : Main update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Just so I can understand better Git Control : Btw Alt + 9 (ç) does open the Version Control Tool Window");
     }
 }
